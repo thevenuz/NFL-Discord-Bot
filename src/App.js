fa5fe9bc333f82fc4936e6c76cf33fd0cfbc43e2
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-01-22 00:46:28 +0530

    change router to hashrouter

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App bg-gray-800 flex flex-col min-h-screen font-Source-Sans text-slate-50">
-      {/* <div className='flex'> */}
         <Header />
-      {/* </div>
-      <div className='flex-1 flex-col h-auto'> */}
         <Routes>
           <Route path="/" element={<PrivacyPolicy />} />
           <Route path="privacy-policy" element={<PrivacyPolicy />} />
           <Route path="terms-of-service" element={<TermsOfService />} />
         </Routes>
-      {/* </div>
-      <div className='flex'> */}
         <Footer />
-      {/* </div> */}
     </div>
   );
 }
